5cd4e8e1c3ec943068075de5b24d4661ca72b5e6
James Oliveira <135138774+JamesOliveira023@users.noreply.github.com>
2023-11-09 11:52:25 -0300

    Atualizado2.2

--- src/componentes/section/Presentation.js
+++ src/componentes/section/Presentation.js
@@ -7,7 +7,8 @@ function Presentation (){
         <div className={style.Presentation} id='Presentation'>
         <h4><strong>Bem vindo ao meu portfólio</strong></h4>
         <h1>Olá, eu sou James Oliveira!</h1>
-        <p>TEXTO SOBRE MIM.<br/>
+        <p>Estudante de Deselvolvimento WEB.FULL STACK James Oliveira.<p>
+        Quero usar as minhas habilidades Programação e edição em HTML, CSS, REACT e demais.</p>  Desenvolvi um projeto para iniciar minha busca por migração de área, para a área da tecnologia.<br/>
         </p>
         <ButtonA link='https://github.com/JamesOliveira023' text='Conecte-se comigo!'/>
         
